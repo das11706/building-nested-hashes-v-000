eaf05f7faf1829e37736b9ecd54973f0e89c1913
Dan Suarez <suarez.daniel101@gmail.com>
2019-03-01 23:58:04 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,10 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
+       {}
      },
      :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :heroine => {},
-       :hero_friends => {}
+       {}
      }
    }
 end
